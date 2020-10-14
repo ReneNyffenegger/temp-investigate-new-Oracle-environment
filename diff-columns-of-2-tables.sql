fa6d3330a112d0cceea79ad75fecd0e753f6f3a3
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-10-14 20:24:34 +0200

    Create diff-columns-of-2-tables.sql

---
+++ diff-columns-of-2-tables.sql
@@ -0,0 +1,15 @@
+with
+   a as (select column_name, column_id from all_tab_columns where table_name = 'T1' and owner = 'OW'),
+   b as (select column_name, column_id from all_tab_columns where table_name = 'T2' and owner = 'OW')
+select
+   a.column_name,
+   b.column_name,
+   a.column_id,
+   b.column_id
+from
+   a full outer join b on a.column_name = b.column_name
+where
+   a.column_name is null or
+   b.column_name is null
+order by
+   a.column_id;
